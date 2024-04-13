55f44f5af1132c7d2e0c45261aaa7b3fc8e66c65
DivyashreeHN <divyagowdahn@gmail.com>
2024-04-13 10:53:09 +0530

    adding frontend part

---
+++ Frontend/bloodbondapp/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import ProfileContainer from './components/profileContainer';
+function App() {
+  return (
+    <div className="App">
+      <h1>Blood Bond</h1>
+      <ProfileContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/bloodbondapp/src/components/profileContainer.js
@@ -0,0 +1,12 @@
+import ProfileForm from "./profileForm"
+export default function ProfileContainer()
+{
+    return(
+        <div className="row">
+            <div className="col-md-3">
+            <ProfileForm/>
+            </div>
+            
+        </div>
+    )
+}
